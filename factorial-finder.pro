f14374dacca68cff1078f9f8c5822fdeaffc769f
aczarnas <adam.czarnas@gmail.com>
2017-09-23 17:35:20 +0200

    Exception for input added

--- factorial-finder.pro
+++ factorial-finder.pro
@@ -3,11 +3,7 @@ CONFIG += console c++11
 CONFIG -= app_bundle
 CONFIG -= qt
 
-SOURCES += "/src/main.cpp" \
-    "/src/finder.cpp" \
-    src/finder.cpp \
+SOURCES += src/finder.cpp \
     src/main.cpp
 
-HEADERS += \
-    /src/finder.h \
-    src/finder.h
+HEADERS += src/finder.h
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <stdexcept>
+#include <string>
 #include "finder.h"
 
 using namespace std;
 
 int main()
 {
-    cout << factorialByLoop(5) << endl;
+    cout << "Factorial base number: ";
+    int baseNumber;
+    string input;
+    cin >> input;
+    try {
+        baseNumber = stoi(input);
+        if(baseNumber < 0) {
+            throw invalid_argument("Negative integer");
+        }
+    }
+    catch (invalid_argument& ia) {
+        cout << input << " is not a positive integer number"  << endl;
+        return -1;
+    }
+    cout << "Number read from input stream: " << baseNumber << endl;
+    cout << "Factorial of number above is: " << factorialByLoop(baseNumber) << endl;
+
     return 0;
 }
